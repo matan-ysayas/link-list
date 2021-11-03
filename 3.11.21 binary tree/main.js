876eec5ee321112a98f54d8d70ec1cf317a0b514
matan-ysayas <matan.ysayas@gmail.com>
2021-11-03 13:44:45 +0200

    binary search

---
+++ 3.11.21 binary tree/main.js
@@ -0,0 +1,25 @@
+const array = [1, 5, 6, 10, 13, 14, 18, 20, 25, 30, 40, 50];
+
+function binarySearch(someArray, num) {    
+  if (someArray.length == 0) {                   //!בדיקה האם המערך תקין במידה ולא הפונקציה יוצאת ומחזירה לא נמצא
+    console.log(`${num} is not found`);
+    return false;
+  }
+
+  let mid = someArray[Math.floor(someArray.length / 2)];//! מוצא את האיבר האמצעי במערך הנתון
+  let leftArray = someArray.slice(0, someArray.length / 2);//! המערך השמאלי מאיבר הראשון ועד האמצעי
+  let rightArray = someArray.slice(someArray.length / 2);//!המערך הימני מהאיבר האמצעי ועד הסוף
+
+  if (num == mid ) { //! במידה והמספר שאנחנו מחפשים שווה לאיבר האמצעי הפונקציה מחזירה שהוא קיים ויוצאת
+    console.log(`${num} is true `);
+    return true;
+  }
+
+  if (num >leftArray[leftArray.length-1]) {//!  במידה והמספר שהכנסנו  קטן מהאיבר האחרון במערך השמאלי מפעילים את הפונקציה על מערך הימני
+    return binarySearch(rightArray, num);
+  } else {
+    return binarySearch(leftArray, num);
+  }
+}
+
+binarySearch(array, -1);
